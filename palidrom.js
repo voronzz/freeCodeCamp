48b92ee51321197be24a4c6b09a1198e8d4019f7
kirill <da.deffoxz@gmail.com>
2023-12-17 18:23:37 +0300

    completed task with palindrome

---
+++ palidrom.js
@@ -0,0 +1,22 @@
+function palindrome(str) {
+    let strN = str.replace(/[\W_]/g, '').toLowerCase();
+
+    if(strN === strN.split('').reverse().join('')) {
+        return true
+    }
+    return false;
+}
+
+palindrome("eye");
+palindrome("_eye");
+palindrome("race car");
+palindrome("not a palindrome");
+palindrome("A man, a plan, a canal. Panama");
+palindrome("never odd or even");
+palindrome("nope");
+palindrome("almostomla");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("My age is 0, 0 si ega ym.");
+palindrome("1 eye for of 1 eye.");
+palindrome("0_0 (: /-\ :) 0-0");
+palindrome("five|\_/|four");
